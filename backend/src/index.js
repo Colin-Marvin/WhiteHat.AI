e0dd2db7859c4a717f4598e34943b42ae63430b9
Colin Marvin <colinmar@umich.edu>
2024-06-30 19:38:17 -0400

    First commit

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+const connectDB = require("./database/db");
+require("dotenv").config();
+connectDB();
+
+const app = express();
+
+// Enabling CORS for any unknown origin
+app.use(cors());
+
+// Enabling the use of JSON for the body of the request
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`IX blogging app listening on port ${port}`);
+});
